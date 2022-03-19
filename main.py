e5fc9e9f597bc622031366fd6ff9b1469790c2b3
FariasMarina <fariasmarinalr@gmail.com>
2022-03-19 12:47:30 -0300

    Getting website status code

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello, World!')
+import urllib.request
+
+
+def main():
+    website = input('[+] Website URL: ')
+    print(urllib.request.urlopen(website).getcode())
+
+
+if __name__ == '__main__':
+    main()
