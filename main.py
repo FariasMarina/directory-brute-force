6a9d70996c9110eef25370359d5dab7624ce6e75
FariasMarina <fariasmarinalr@gmail.com>
2022-04-15 11:33:56 -0300

    verificando o status code de um website com diretórios mockados2

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import urllib.request
 
-
 def main():
+    words = ['admin', 'contact', 'index']
     website = input('[+] Website URL: ')
-    print(urllib.request.urlopen(website).getcode())
+
+    for word in words:
+        directories = website + word
+        try:
+            print(urllib.request.urlopen(directories).getcode())
+        except Exception as error:
+            print(error)
 
 
 if __name__ == '__main__':
