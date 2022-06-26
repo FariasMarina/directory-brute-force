d18a72d059da5b3c8149fc9e30d078971e29c9f5
FariasMarina <fariasmarinalr@gmail.com>
2022-06-26 13:31:50 -0300

    MVP Created

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
 import urllib.request
+from os import path
+import datetime
+
+"""
+TODO: 
+- Recognize Wordpress URL and use wordpress wordlists;
+- Use default wordlist if none is inserted;
+- Insert https or http if URL doesn't have it;
+- Customize output colors;
+- Brute-force with more threads; 
+"""
 
 def main():
-    words = ['admin', 'contact', 'index']
     website = input('[+] Website URL: ')
+    words_dir = input('[+] Wordlist path: ')
 
-    for word in words:
-        directories = website + word
+    print_program_info()
+
+    words = path.realpath(words_dir)
+
+    with open(words) as file:
+        lines = file.readlines()
+        lines = [line.rstrip() for line in lines]
+
+    for word in lines:
+        directory = website + word
         try:
-            print(urllib.request.urlopen(directories).getcode())
+            print(f'[{urllib.request.urlopen(directory).getcode()}]  /{word} ')
         except Exception as error:
-            print(error)
+            pass
 
+    print(40*"═")
+    now = datetime.datetime.now()
+    print(f'Successfully concluded at {now.hour}:{now.minute}:{now.second}')
+    print(40*"═")
+    
+
+def print_program_info():
+    print(40*"═")
+    print('DBT - v1.1.0')
+    print('by Marina Farias (@dilunno)')
+    print(40*"═")
+    now = datetime.datetime.now()
+    print(f'Started at {now.hour}:{now.minute}:{now.second}')
+    print(40*"═")
+ 
 
 if __name__ == '__main__':
     main()
